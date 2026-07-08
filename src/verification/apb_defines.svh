95760f8b04a609d6af586b500b17271c0b614435
shreeshakumar <shreeshamatandoor@gmail.com>
2026-07-09 03:29:32 +0530

    5th

--- src/verification/apb_defines.svh
+++ src/verification/apb_defines.svh
@@ -1,2 +1,4 @@
 `define ADDR_WIDTH 5
 `define DATA_WIDTH 8
+
+`define no_of_trans 10
---
+++ src/verification/apb_interface.sv
@@ -0,0 +1,62 @@
+interface apb_inf(input logic PCLK,PREASTn);
+	
+		//inputs from user to master
+		logic 						transfer, write_read;
+		logic [`ADDR_WIDTH-1:0]		addr_in;
+		logic [`DATA_WIDTH-1:0]		wdata_in;
+		logic [(`DATA_WIDTH/8)-1:0]	strb_in;
+		
+		//outputs to user from master
+		logic [`DATA_WIDTH-1:0]		rdata_out;
+		logic 						transfer_done, error;
+	
+		//outputs to slave from master
+		logic [`ADDR_WIDTH-1:0]		PADDR;
+		logic 						PSEL, PENABLE, PWRITE;
+		logic [`DATA_WIDTH-1:0]		PWDATA;
+		logic [(`DATA_WIDTH/8)-1:0]	PSTRB;
+	
+		//inputs from slave to master
+		logic [`DATA_WIDTH-1:0]		PRDATA;
+		logic 						PREADY, PSLVERR;
+
+		clocking cb_driver@(posedge PCLK);
+			default input #0 output #0;
+			output transfer, write_read, addr_in, wdata_in, strb_in;
+			input  rdata_out, transfer_done, error;
+			
+			input PADDR, PSEL, PENABLE, PWRITE, PWDATA, PSTRB;
+			output PRDATA, PREADY, PSLVERR;		
+			
+			input reset;
+						
+		endclocking
+
+		clocking cb_reference@(posedge PCLK);
+			default input #0 output #0;
+			output transfer, write_read, addr_in, wdata_in, strb_in;
+			input  rdata_out, transfer_done, error;
+			
+			input PADDR, PSEL, PENABLE, PWRITE, PWDATA, PSTRB;
+			output PRDATA, PREADY, PSLVERR;
+			
+			input reset;
+				
+		endclocking
+
+		clocking cb_monitor@(posedge clk);
+			default input #0 output #0;
+			input transfer, write_read, addr_in, wdata_in, strb_in;
+			input rdata_out, transfer_done, error;
+			input PADDR, PSEL, PENABLE, PWRITE, PWDATA, PSTRB;
+			input PRDATA, PREADY, PLVERR;
+
+			input reset;
+			
+		endclocking
+
+		modport DRIVER(clocking cb_driver);
+		modport REFERENCE(clocking cb_reference);
+		modport MONITOR(clocking cb_monitor);
+
+endinterface
